71161b2006e12fa5014173dc3abb42c8bc509f3f
Mykhaylo Lelgant <mykle23.dev@gmail.com>
2024-11-29 13:55:05 +0100

    first test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,6 +1,10 @@
 import { it, expect } from "vitest";
 import { add } from "./math";
 it("should summarize all number values in a array", () => {
-  const result = add([0, 1, 2, 3, 4]);
+  // Arrange
+  const numbers = [0, 1, 2, 3, 4];
+  // Act
+  const result = add(numbers);
+  // Assert
   expect(result).toBe(10);
 });
